57716e88b47b5438f6ecf97b4497d6ec255363dd
github username <github email address>
2020-04-28 20:12:28 +0100

    prompt-attempt

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,36 @@
 class AnswersController < ApplicationController
+    def index 
+        answers = Answer.all
+        render json: answers
+    end 
+
+    def show 
+        answer = Answer.find(params[:id])
+        render json: answer
+    end 
+
+    def create
+        answer = Answer.create(answer_params)
+        if (answer.valid?)
+        render json: answer
+        else 
+        render json: {error: "error"}
+        end
+    end
+    
+    def update
+        answer = Answer.find(params[:id])
+        answer.update(journal_entry: params[:journal_entry])
+        render json: answer, include: [:mood, :user]
+    end
+    
+    # def destroy
+    #     entry = Entry.find(params[:id])
+    #     entry.destroy
+    # end
+
+    private 
+    def answer_params
+        params.require(:entry).permit(:question_answer, :question_id)
+    end 
 end
